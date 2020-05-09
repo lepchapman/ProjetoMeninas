761063bc216758d4aa29b311fc26f8a566faacd1
nayaradias <nayaradias@users.noreply.github.com>
2020-05-09 18:41:06 -0300

    Adicionando o app component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html'
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styles: [
+    `
+      .container .row {
+        margin: 120px auto;
+        width: 60%;
+      }
+
+      a.link-nav {
+        text-decoration: none;
+        cursor: pointer;
+        margin: 0 90px;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-    
-}
+export class AppComponent {}
